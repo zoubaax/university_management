0b18f10915f3b715a5bca16e5a42a5fbdbcb1dfd
zoubaax <zoubaax@gmail.com>
2026-07-06 23:41:08 +0100

    refactor: simplify cypress configuration by removing defineConfig wrapper

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,4 @@
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
+export default {
   e2e: {
     baseUrl: 'http://localhost:5173',
     supportFile: false,
@@ -9,4 +7,4 @@ export default defineConfig({
       // implement node event listeners here
     },
   },
-});
+};
